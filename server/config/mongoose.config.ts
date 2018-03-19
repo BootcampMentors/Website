fb93e7836e55af7f5ccef9ba57476eb52a5644ec
EmilChoparinov <emilchoparinov@gmail.com>
2018-03-19 13:18:06 -0700

    require changed to js no ts

--- server/config/mongoose.config.ts
+++ server/config/mongoose.config.ts
@@ -5,7 +5,7 @@ import { join as joinPaths } from 'path';
 mongoose.connect('mongodb://localhost/bootcampmentors');
 
 readdirSync(joinPaths(__dirname, './../models/')).forEach((model) => {
-    if (model.indexOf('.ts') > 0) {
+    if (model.indexOf('.js') > 0) {
         require(joinPaths(__dirname, `./../models/${model}`));
     }
 });
